106e49f09fea759587af9a9bc2fe3354276293af
Devolpment <devolpment@MacBook-Pro-3.local>
2024-07-11 17:49:08 -0700

    refactor the code in the set and has function

--- index.js
+++ index.js
@@ -23,39 +23,38 @@ class HashMap {
     if (this._arraySet[index]) {
       this._arraySet[index].forEach((element) => {
         if (element.key === key) element.value = value;
-        else this._arraySet[index].append({ key, value });
+        else this._arraySet[index].push({ key, value });
       });
     } else {
       this._arraySet[index] = [{ key, value }];
+      this._arraySize++;
     }
-
-    this._arraySize++;
   }
 
   get(key) {
     let index = this.#hash(key);
-    if (this._arraySet[index].length === 1) return this._arraySet[index][0];
+    if (!this._arraySet[index]) return false;
+    else if (this._arraySet[index][0].key === key)
+      return this._arraySet[index][0];
+
     for (let element of this._arraySet[index]) {
       if (element.key === key) return element;
     }
   }
 
   has(key) {
-    for (let element of this._arraySet) {
-      if (element) return element[0].key === key;
-      else if (element && element.length > 1) {
-        for (let objects of element) {
-          return objects.key === key;
-        }
-      }
-    }
+    if (this.get(key)) return true;
+
+    return false;
+  }
+
+  remove(key) {
+    let index = this.#hash(key);
+    if (this._arraySet[index][0].key === key) this._arraySet[index] = [];
   }
 }
 
 let test = new HashMap();
-test.set("Carlos", "this is a value");
-test.set("Carlos", "this is a new value");
-test.set("Carla", "this is a carla value");
-test.set("Carlaa", "this is a carla value");
-console.log(test._arraySet);
-console.log(test.has("Carlaa"));
+test.set("A", "this is A");
+test.set("Q", "this is Q");
+console.log(test.has("Z"));
